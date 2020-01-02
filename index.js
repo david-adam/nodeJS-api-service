b0ed2d37dda4808451fffc6be500de6c2d22383e
David Dai <wdai1@paypal.com>
2020-01-02 16:01:16 +0800

    add a messages service

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ import express from 'express';
 
 const app = express();
 
+const messages = [
+  {
+    text: 'Some messages',
+    owner: 'David'
+  },
+  {
+    text: 'the seconde messages',
+    owner: 'Jim'
+  }
+];
+
 app.get('/', (_req, res) => {
   res.json('Hello');
+});
+
+app.get('/messages', (req, res) => {
+  res.json(messages);
 });
 
 const port = process.env.port || 8000;
